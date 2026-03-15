f222ee63e0441c6ae57602405cbd61d8473312c2
Lucas Machiner <machiner@cbm.sc.gov.br>
2026-03-15 16:18:10 -0300

    feat: enhance parseVisitTimestamp function for better timestamp handling

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,44 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const parseVisitTimestamp = (timestamp: unknown): Date => {
+  if (timestamp instanceof Date && !Number.isNaN(timestamp.getTime())) {
+    return timestamp;
+  }
+
+  if (timestamp && typeof timestamp === 'object') {
+    const firestoreTimestamp = timestamp as {
+      toDate?: () => Date;
+      seconds?: number;
+      _seconds?: number;
+    };
+
+    if (typeof firestoreTimestamp.toDate === 'function') {
+      const converted = firestoreTimestamp.toDate();
+      if (converted instanceof Date && !Number.isNaN(converted.getTime())) {
+        return converted;
+      }
+    }
+
+    const seconds = typeof firestoreTimestamp.seconds === 'number'
+      ? firestoreTimestamp.seconds
+      : firestoreTimestamp._seconds;
+
+    if (typeof seconds === 'number') {
+      const converted = new Date(seconds * 1000);
+      if (!Number.isNaN(converted.getTime())) {
+        return converted;
+      }
+    }
+  }
+
+  if (typeof timestamp === 'string' || typeof timestamp === 'number') {
+    const converted = new Date(timestamp);
+    if (!Number.isNaN(converted.getTime())) {
+      return converted;
+    }
+  }
+
+  return new Date();
+};
